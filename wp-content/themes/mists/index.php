655c626608bb1e22e32ee8e5dfe3fc9c15c56d0b
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-02-02 11:43:19 -0500

    Child Theme was made.

--- wp-content/themes/mists/index.php
+++ wp-content/themes/mists/index.php
@@ -20,25 +20,29 @@
             <li>Feruchemy</li>
         </ul>
     </nav>
-    <h1>Mists</h1>
+    <div class="mainDiv">
+        <h1>Mists</h1>
 
-    <?php
-    if (have_posts()) :
-        while (have_posts()) :
-            the_post()
-    ?>
-            <h4>
-                <?php
-                the_title();
-                ?>
-            </h4>
-            <p><i>By: <?php the_author() ?></i> (<?php the_date() ?>)</p>
-            <p><?php the_excerpt() ?></p>
-    <?php
-        endwhile;
 
-    endif;
-    ?>
+
+        <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post()
+        ?>
+                <h4>
+                    <?php
+                    the_title();
+                    ?>
+                </h4>
+                <p><i>By: <?php the_author() ?></i> (<?php the_date() ?>)</p>
+                <p><?php the_excerpt() ?></p>
+        <?php
+            endwhile;
+
+        endif;
+        ?>
+    </div>
     <?php wp_footer() ?>
 </body>
 
